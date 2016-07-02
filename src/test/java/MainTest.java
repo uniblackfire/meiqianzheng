9ba0ef21cb65caefb04e31933204aa0b663cdb06
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-07-02 13:23:33 +0800

    initial project

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MainTest {
+
+    @Test
+    public void should_return_hello_world_when_store_received_commands() throws Exception {
+        // given
+        
+        // when
+
+        // then
+        assertThat(1 + 1, is(2));
+    }
+}
